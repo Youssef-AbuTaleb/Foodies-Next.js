bce917dad47742de2d6dd338b37d2cea10ff2c57
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-03-05 21:38:27 +0200

    add sqlite database and use it to fetch all meals in meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,28 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/share">Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/m1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/m2">Meal 2</Link>
-      </p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is asy and fun!
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">Share your favorite recipe</Link>
+          </p>
+        </h1>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 }
