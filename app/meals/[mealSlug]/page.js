7f96de612c70ad9cac29ac9e71ff7839b4ff0856
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-03-06 00:57:14 +0200

    use notFound method on meal page to fallback to the nearst not-found page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default async function MealsDetailsPage({ params }) {
   const meal = await getMeal(params.mealSlug);
 
-  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  if (!meal) {
+    notFound();
+  }
 
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
       <header className={classes.header}>
--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,8 @@
-export default function Custom404() {
+export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Not found</h1>
-      <p>Unfortunately, we could not find the requested page ore resource.</p>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
     </main>
   );
 }
